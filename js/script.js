6ef87a13294ae3298d3907ec4a6fd81bed108417
RenatoConfuorto <renato.confuorto@outlook.it>
2022-05-05 17:03:42 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -3,6 +3,15 @@ Vue.config.devtools = true;
 const app = new Vue ({
     el: '#root',
     data:{
-      title: 'Ciao'
+      title: 'Ciao',
+      image: 'https://unsplash.it/300/300?image',
+      imageWidth: 'auto',
+      imageHeight: 'auto'
     },
+    methods:{
+      resetImage: function(){
+        this.imageWidth = 'auto';
+        this.imageHeight = 'auto';
+      }
+    }
 });
